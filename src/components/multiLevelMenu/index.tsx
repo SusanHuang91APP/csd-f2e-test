6c230fa520e9bb3de500b287f2d039e8b12c07c1
趙世平 <sam_chao@i-planet.com.tw>
2024-08-01 15:41:09 +0800

    -modify MultiLevelMenu:將parentCategories改成initialParents SingleLevelMenu:將子層遞迴的parentCategories改成childrenParentList

--- src/components/multiLevelMenu/index.tsx
+++ src/components/multiLevelMenu/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { TCategory } from "@/types/category.type";
 import SingleLevelMenu from "@/components/multiLevelMenu/singleLevelMenu";
 import { Box, MenuList, Paper } from "@mui/material";
@@ -18,52 +18,87 @@ const fetchCategories = async () => {
 interface MultiLevelMenuProps {
   onSelect: (categories: TCategory[]) => void;
 }
-const findCategoryById = (
-  categories: TCategory[],
-  id: number
-): TCategory | null => {
-  for (const category of categories) {
-    //從categories第一層開始找是否符合傳入的parentId
-    if (category.Id === id) {
-      return category;
-    }
-    //不符合就從ChildList開始遞迴找
-    const childCategory = findCategoryById(category.ChildList, id);
-    if (childCategory) {
-      return childCategory;
-    }
+// const findCategoryById = (
+//   categories: TCategory[],
+//   id: number
+// ): TCategory | null => {
+//   for (const category of categories) {
+//     //從categories第一層開始找是否符合傳入的parentId
+//     if (category.Id === id) {
+//       return category;
+//     }
+//     //不符合就從ChildList開始遞迴找
+//     const childCategory = findCategoryById(category.ChildList, id);
+//     if (childCategory) {
+//       return childCategory;
+//     }
+//   }
+//   return null;
+// };
+
+// const getCategoryAncestors = (
+//   categories: TCategory[],
+//   category: TCategory
+// ): TCategory[] => {
+//   const ancestors: TCategory[] = [];
+//   let currentCategory: TCategory | null = category;
+
+//   //如果點擊的是第二層之後
+//   while (currentCategory && currentCategory.Level > 1) {
+//     ancestors.unshift(currentCategory);
+//     currentCategory = findCategoryById(categories, currentCategory.ParentId);
+//   }
+//   //如果點擊的是第一層
+//   if (currentCategory) {
+//     ancestors.unshift(currentCategory);
+//   }
+//   return ancestors;
+// };
+const MultiLevelMenu: React.FC<MultiLevelMenuProps> = ({ onSelect }) => {
+  const [categories, setCategories] = useState<TCategory[]>([]);
+  const [openCategories, setOpenCategories] = useState<number[]>([]);
+  const handleClickCategory = useCallback(
+    (categories: TCategory[]) => {
+      const openedIds = categories.map((item) => item.Id);
+      setOpenCategories(openedIds);
+      onSelect(categories);
+    },
+    [setOpenCategories, onSelect]
+  );
+  const initialParents = useMemo(() => [], []);
+  useEffect(() => {
+    fetchCategories().then((data) => setCategories(data || []));
+  }, []);
+  {
+    /* TODO: 需求1：請依照 Api Response 實作多層分類選單元件(MultiLevelMenu) */
   }
-  return null;
+  return (
+    <MenuList>
+      {categories.map((category) => (
+        <SingleLevelMenu
+          key={category.Id}
+          category={category}
+          parentCategories={initialParents}
+          openCategories={openCategories}
+          onClickCategory={handleClickCategory}
+        />
+      ))}
+    </MenuList>
+  );
 };
 
-const getCategoryAncestors = (
-  categories: TCategory[],
-  category: TCategory
-): TCategory[] => {
-  const ancestors: TCategory[] = [];
-  let currentCategory: TCategory | null = category;
-
-  //如果點擊的是第二層之後
-  while (currentCategory && currentCategory.Level > 1) {
-    ancestors.unshift(currentCategory);
-    currentCategory = findCategoryById(categories, currentCategory.ParentId);
-  }
-  //如果點擊的是第一層
-  if (currentCategory) {
-    ancestors.unshift(currentCategory);
-  }
-  return ancestors;
-};
 // const MultiLevelMenu: React.FC<MultiLevelMenuProps> = ({ onSelect }) => {
 //   const [categories, setCategories] = useState<TCategory[]>([]);
 //   const [openCategories, setOpenCategories] = useState<number[]>([]);
+
 //   const handleClickCategory = useCallback(
-//     (categories: TCategory[]) => {
-//       const openedIds = categories.map((item) => item.Id);
+//     (category: TCategory) => {
+//       const ancestors = getCategoryAncestors(categories, category);
+//       onSelect(ancestors);
+//       const openedIds = ancestors.map((item) => item.Id);
 //       setOpenCategories(openedIds);
-//       onSelect(categories);
 //     },
-//     [setOpenCategories, onSelect]
+//     [setOpenCategories, onSelect, categories]
 //   );
 //   useEffect(() => {
 //     fetchCategories().then((data) => setCategories(data || []));
@@ -77,50 +112,17 @@ const getCategoryAncestors = (
 //         <SingleLevelMenu
 //           key={category.Id}
 //           category={category}
-//           parentCategories={[]}
-//           openCategories={openCategories}
 //           onClickCategory={handleClickCategory}
+//           openCategories={openCategories}
 //         />
 //       ))}
 //     </MenuList>
 //   );
 // };
 
-const MultiLevelMenu: React.FC<MultiLevelMenuProps> = ({ onSelect }) => {
-  const [categories, setCategories] = useState<TCategory[]>([]);
-  const [openCategories, setOpenCategories] = useState<number[]>([]);
-
-  const handleClickCategory = useCallback(
-    (category: TCategory) => {
-      const ancestors = getCategoryAncestors(categories, category);
-      onSelect(ancestors);
-      const openedIds = ancestors.map((item) => item.Id);
-      setOpenCategories(openedIds);
-    },
-    [setOpenCategories, onSelect, categories]
-  );
-  useEffect(() => {
-    fetchCategories().then((data) => setCategories(data || []));
-  }, []);
-  {
-    /* TODO: 需求1：請依照 Api Response 實作多層分類選單元件(MultiLevelMenu) */
-  }
-  return (
-    <MenuList>
-      {categories.map((category) => (
-        <SingleLevelMenu
-          key={category.Id}
-          category={category}
-          onClickCategory={handleClickCategory}
-          openCategories={openCategories}
-        />
-      ))}
-    </MenuList>
-  );
-};
-
 export default MultiLevelMenu;
 
 /*
-
+將註解掉版本的改成parentCategories={initialParents}
+const initialParents = useMemo(() => [], []);
 */
--- src/components/multiLevelMenu/singleLevelMenu/index.tsx
+++ src/components/multiLevelMenu/singleLevelMenu/index.tsx
@@ -1,25 +1,93 @@
 import type { TCategory } from "@/types/category.type";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 import { MenuItem, Collapse, List, ListItemText, Box } from "@mui/material";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 
+type SingleLevelMenuProps = {
+  category: TCategory;
+  parentCategories: TCategory[];
+  openCategories: number[];
+  onClickCategory: (categories: TCategory[]) => void;
+};
+const SingleLevelMenu: FC<SingleLevelMenuProps> = React.memo(
+  function SingleLevelMenu({
+    category,
+    parentCategories,
+    openCategories,
+    onClickCategory,
+  }) {
+    const isOpen = useMemo(() => {
+      return openCategories.includes(category.Id);
+    }, [openCategories, category.Id]);
+    const handleClick = () => {
+      onClickCategory([...parentCategories, category]);
+    };
+    const childrenParentList = useMemo(
+      () => [...parentCategories, category],
+      [category, parentCategories]
+    );
+    useEffect(() => {
+      console.log(category.Name);
+    });
+    return (
+      <Box>
+        <MenuItem
+          onClick={handleClick}
+          sx={{
+            pl: category.Level * 2,
+          }}
+        >
+          <ListItemText primary={category.Name} />
+          {category.ChildListCount > 0 ? (
+            isOpen ? (
+              <ExpandLess />
+            ) : (
+              <ExpandMore />
+            )
+          ) : null}
+        </MenuItem>
+        {category.ChildListCount > 0 && (
+          <Collapse in={isOpen} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {category.ChildList.map((child) => (
+                <SingleLevelMenu
+                  key={child.Id}
+                  category={child}
+                  parentCategories={childrenParentList}
+                  openCategories={openCategories}
+                  onClickCategory={onClickCategory}
+                />
+              ))}
+            </List>
+          </Collapse>
+        )}
+      </Box>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.openCategories.length === nextProps.openCategories.length &&
+      prevProps.openCategories.every(
+        (Id, index) => Id === nextProps.openCategories[index]
+      ) &&
+      prevProps.category.Id === nextProps.category.Id &&
+      prevProps.parentCategories === nextProps.parentCategories &&
+      prevProps.onClickCategory === nextProps.onClickCategory
+    );
+  }
+);
+
 // type SingleLevelMenuProps = {
 //   category: TCategory;
-//   parentCategories: TCategory[];
+//   onClickCategory: (category: TCategory) => void;
 //   openCategories: number[];
-//   onClickCategory: (categories: TCategory[]) => void;
 // };
 // const SingleLevelMenu: FC<SingleLevelMenuProps> = React.memo(
-//   function SingleLevelMenu({
-//     category,
-//     parentCategories,
-//     openCategories,
-//     onClickCategory,
-//   }) {
+//   function SingleLevelMenu({ category, onClickCategory, openCategories }) {
 //     const isOpen = openCategories.includes(category.Id);
 //     const handleClick = () => {
-//       onClickCategory([...parentCategories, category]);
+//       onClickCategory(category);
 //     };
 //     return (
 //       <Box>
@@ -45,9 +113,8 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 //                 <SingleLevelMenu
 //                   key={child.Id}
 //                   category={child}
-//                   parentCategories={[...parentCategories, category]}
-//                   openCategories={openCategories}
 //                   onClickCategory={onClickCategory}
+//                   openCategories={openCategories}
 //                 />
 //               ))}
 //             </List>
@@ -63,67 +130,19 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 //         (Id, index) => Id === nextProps.openCategories[index]
 //       ) &&
 //       prevProps.category.Id === nextProps.category.Id &&
-//       prevProps.parentCategories === nextProps.parentCategories &&
 //       prevProps.onClickCategory === nextProps.onClickCategory
 //     );
 //   }
 // );
 
-type SingleLevelMenuProps = {
-  category: TCategory;
-  onClickCategory: (category: TCategory) => void;
-  openCategories: number[];
-};
-const SingleLevelMenu: FC<SingleLevelMenuProps> = React.memo(
-  function SingleLevelMenu({ category, onClickCategory, openCategories }) {
-    const isOpen = openCategories.includes(category.Id);
-    const handleClick = () => {
-      onClickCategory(category);
-    };
-    return (
-      <Box>
-        <MenuItem
-          onClick={handleClick}
-          sx={{
-            pl: category.Level * 2,
-          }}
-        >
-          <ListItemText primary={category.Name} />
-          {category.ChildListCount > 0 ? (
-            isOpen ? (
-              <ExpandLess />
-            ) : (
-              <ExpandMore />
-            )
-          ) : null}
-        </MenuItem>
-        {category.ChildListCount > 0 && (
-          <Collapse in={isOpen} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              {category.ChildList.map((child) => (
-                <SingleLevelMenu
-                  key={child.Id}
-                  category={child}
-                  onClickCategory={onClickCategory}
-                  openCategories={openCategories}
-                />
-              ))}
-            </List>
-          </Collapse>
-        )}
-      </Box>
-    );
-  },
-  (prevProps, nextProps) => {
-    return (
-      prevProps.openCategories.length === nextProps.openCategories.length &&
-      prevProps.openCategories.every(
-        (Id, index) => Id === nextProps.openCategories[index]
-      ) &&
-      prevProps.category.Id === nextProps.category.Id &&
-      prevProps.onClickCategory === nextProps.onClickCategory
-    );
-  }
-);
-
 export default SingleLevelMenu;
+
+/*
+將註解掉版本的改成parentCategories={initialParents}
+const initialParents = useMemo(() => [], []);
+const childrenParentList = useMemo(() => 
+    [...parentCategories, category],
+    [category.Id, parentCategories]
+  );
+parentCategories={childrenParentList}
+*/
